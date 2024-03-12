7e9704afdbbb4b6b4536533d7e877d833403402e
Diego <diegoolivera027@gmail.com>
2024-03-12 15:12:28 +1300

    Test

--- src/cosc202/andie/ConvertToGrey.java
+++ src/cosc202/andie/ConvertToGrey.java
@@ -7,6 +7,8 @@ import java.awt.image.*;
  * ImageOperation to convert an image from colour to greyscale.
  * </p>
  * 
+ * this y your granny dead
+ * 
  * <p>
  * The images produced by this operation are still technically colour images,
  * in that they have red, green, and blue values, but each pixel has equal
