1025c2f17731509a7a972302608ebc5e7c4d6bc4
Jessica Fan <jessicafan2603@gmail.com>
2024-03-18 22:34:55 +1300

    Gaussian Blur Class

--- src/cosc202/andie/FilterActions.java
+++ src/cosc202/andie/FilterActions.java
@@ -34,11 +34,10 @@ public class FilterActions {
      */
     public FilterActions() {
         actions = new ArrayList<Action>();
-        actions.add(new MeanFilterAction("Mean filter", null, "Apply a mean filter", Integer.valueOf(KeyEvent.VK_M)));
-
+        actions.add(new MeanFilterAction("Mean Filter", null, "Apply a mean filter", Integer.valueOf(KeyEvent.VK_M)));
+        actions.add(new GaussianBlurAction("Gaussian Blur", null, "Apply a gaussian blur", Integer.valueOf(KeyEvent.VK_G)));
         actions.add(new SoftBlurAction("Soft Blur", null, "Apply a soft blur", Integer.valueOf(KeyEvent.VK_B)));
-        actions.add(new SoftBlurAction("Sharpen Image", null, "Sharpen the Image", Integer.valueOf(KeyEvent.VK_A)));
-
+        actions.add(new SharpenImageAction("Sharpen Image", null, "Apply a sharpen filter", Integer.valueOf(KeyEvent.VK_A)));
     }
 
     /**
@@ -118,6 +117,47 @@ public class FilterActions {
 
     }
 
+    public class GaussianBlurAction extends ImageAction {
+
+        /* 
+         * @param name The name of the action (ignored if null).
+         * @param icon An icon to use to represent the action (ignored if null).
+         * @param desc A brief description of the action  (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         */
+        GaussianBlurAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+
+        /**
+         * @param e The event triggering this callback.
+         */
+        public void actionPerformed(ActionEvent e) {
+
+            // Determine the radius - ask the user.
+            int radius = 1;
+
+            // Pop-up dialog box to ask for the radius value.
+            SpinnerNumberModel radiusModel = new SpinnerNumberModel(1, 1, 10, 1);
+            JSpinner radiusSpinner = new JSpinner(radiusModel);
+            int option = JOptionPane.showOptionDialog(null, radiusSpinner, "Enter filter radius", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+            // Check the return value from the dialog box.
+            if (option == JOptionPane.CANCEL_OPTION) {
+                return;
+            } else if (option == JOptionPane.OK_OPTION) {
+                radius = radiusModel.getNumber().intValue();
+            }
+
+            // Create and apply the filter
+            target.getImage().apply(new GaussianBlur(radius));
+            target.repaint();
+            target.getParent().revalidate();
+        }
+
+    }
+
+
     public class SoftBlurAction extends ImageAction{
         SoftBlurAction(String name, ImageIcon icon, String desc, Integer mnemonic){
             super(name, icon, desc, mnemonic);
@@ -141,6 +181,5 @@ public class FilterActions {
             target.getParent().revalidate();
         }
     }
-
 
 }
---
+++ src/cosc202/andie/GaussianBlur.java
@@ -0,0 +1,51 @@
+package cosc202.andie;
+
+import java.awt.image.*;
+
+public class GaussianBlur implements ImageOperation, java.io.Serializable {
+
+    private int radius;
+
+    GaussianBlur(int radius){
+        this.radius = radius;
+    }
+
+    GaussianBlur(){
+        this(1);
+    }
+
+    public BufferedImage apply (BufferedImage input){
+        float[] array = creatingGaussianKernel(radius);
+        
+        Kernel kernel = new Kernel(2*radius+1, 2*radius+1, array);
+        ConvolveOp convOp = new ConvolveOp(kernel);
+        BufferedImage output = new BufferedImage(input.getColorModel(), input.copyData(null), input.isAlphaPremultiplied(), null);
+        convOp.filter(input, output);
+
+        return output;
+    }
+
+    private float[] creatingGaussianKernel(int radius){
+        int size = 2 * radius + 1;
+        float[] array = new float[size * size];
+        double sigma = radius / 3;
+        double frontHalf = 1 / (2 * Math.PI * sigma * sigma);
+        double total = 0;
+
+        for (int x = -radius; x <= radius; x++){
+            for (int y = -radius; y <= radius; y++){
+                int index = (x + radius) * size + (y + radius);
+                double exponent = -(x * x + y * y)/(2 * sigma * sigma);
+                array[index] = (float) frontHalf * (float) Math.exp(exponent);
+                total += array[index];
+            }
+        }
+
+        for (int i = 0; i < array.length; i++){
+            array[i] /= total;
+        }
+
+        return array;
+    }
+
+}
