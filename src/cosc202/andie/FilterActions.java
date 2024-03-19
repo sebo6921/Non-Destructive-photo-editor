92afe1abc5816a427270acd083be4f2b8e487ac8
Kruti Mistry <miskr858@student.otago.ac.nz>
2024-03-19 21:19:47 +1300

    median filter

--- src/cosc202/andie/FilterActions.java
+++ src/cosc202/andie/FilterActions.java
@@ -38,6 +38,8 @@ public class FilterActions {
         actions.add(new GaussianBlurAction("Gaussian Blur", null, "Apply a gaussian blur", Integer.valueOf(KeyEvent.VK_G)));
         actions.add(new SoftBlurAction("Soft Blur", null, "Apply a soft blur", Integer.valueOf(KeyEvent.VK_B)));
         actions.add(new SharpenImageAction("Sharpen Image", null, "Sharpen the Image", Integer.valueOf(KeyEvent.VK_A)));
+        actions.add(new MedianFilterAction("Median Filter", null, "Apply a median filter", Integer.valueOf(KeyEvent.VK_D)));
+
 
     }
 
@@ -181,6 +183,59 @@ public class FilterActions {
             target.repaint();
             target.getParent().revalidate();
         }
+    }
+
+    public class MedianFilterAction extends ImageAction {
+
+        /**
+         * <p>
+         * Create a new median-filter action.
+         * </p>
+         * 
+         * @param name The name of the action (ignored if null).
+         * @param icon An icon to use to represent the action (ignored if null).
+         * @param desc A brief description of the action  (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         */
+        MedianFilterAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+
+        /**
+         * <p>
+         * Callback for when the median filter action is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the MedianFilterAction is triggered.
+         * It prompts the user for a filter radius, then applies an appropriately sized {@link MedianFilter}.
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
+        public void actionPerformed(ActionEvent e) {
+
+            // Determine the radius - ask the user.
+            int radius = 1;
+
+            // Pop-up dialog box to ask for the radius value.
+            SpinnerNumberModel radiusModel = new SpinnerNumberModel(1, 1, 10, 1);
+            JSpinner radiusSpinner = new JSpinner(radiusModel);
+            int option = JOptionPane.showOptionDialog(null, radiusSpinner, "Enter filter radius", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+            // Check the return value from the dialog box.
+            if (option == JOptionPane.CANCEL_OPTION) {
+                return;
+            } else if (option == JOptionPane.OK_OPTION) {
+                radius = radiusModel.getNumber().intValue();
+            }
+
+            // Create and apply the filter
+            target.getImage().apply(new MedianFilter(radius));
+            target.repaint();
+            target.getParent().revalidate();
+        }
+
     }
 
 }
--- src/cosc202/andie/MedianFilter.java
+++ src/cosc202/andie/MedianFilter.java
@@ -3,7 +3,7 @@ package cosc202.andie;
 import java.awt.image.*;
 import java.util.*;
 
-public class MedianFilter implements ImageOperation, java.io.Serializable  {
+public class MedianFilter implements ImageOperation, java.io.Serializable {
     /**
      * The size of filter to apply. A radius of 1 is a 3x3 filter, a radius of 2 a
      * 5x5 filter, and so forth.
@@ -25,23 +25,62 @@ public class MedianFilter implements ImageOperation, java.io.Serializable  {
     public MedianFilter(int radius) {
         this.radius = radius;
     }
-
+    /**
+     * Apply the median filter to the input image.
+     * 
+     * @param input The input image to filter.
+     * @return The filtered output image.
+     */
     public BufferedImage apply(BufferedImage input) {
-        int width = input.getWidth(); // retrieves the width of the BufferedImage parameter
-        int height = input.getHeight(); // retrieves the height of the BufferedImage parameter
+        int width = input.getWidth();
+        int height = input.getHeight();
+        BufferedImage output = new BufferedImage(width, height, input.getType());
 
-        BufferedImage output = new BufferedImage(input.getColorModel(), input.copyData(null), input.isAlphaPremultiplied(), null);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int argb = input.getRGB(x, y);
+                // By shifting the bits appropriately, you can isolate each color channel so that 
+                //you can manipulate or extract it independently.
+                int a = (argb >> 24) & 0xFF; // Alpha channel, shifts to the right by 24 bits
+                int r = (argb >> 16) & 0xFF; // Red channel, shifts to the right by 16 bits
+                int g = (argb >> 8) & 0xFF; // Green channel, shifts tot the right by 8 bits
+                int b = argb & 0xFF; // Blue channel, no need to shift this one
 
-        
+                // applying the median filter to each color channel separately
+                int redMedian = getMedian(input, x, y, 16);
+                int greenMedian = getMedian(input, x, y, 8);
+                int blueMedian = getMedian(input, x, y, 0);
 
-        
+                // adding together the color channels into a single ARGB value
+                int addedArgb = (a << 24) | (redMedian << 16) | (greenMedian << 8) | blueMedian;
 
+                
+                output.setRGB(x, y, addedArgb);
+            }
+        }
 
-
-
-
-
-
-}
-
+        return output;
+    }
+     /**
+     * finds the median value for a given pixel location and colour channel shift.
+     * 
+     * @param input The input image.
+     * @param x     The x-coordinate of the pixel.
+     * @param y     The y-coordinate of the pixel.
+     * @param shift The bit shift value for the color channel.
+     * @return The median value for the specified color channel.
+     */
+    private int getMedian(BufferedImage input, int x, int y, int shift) {
+        int[] filterSize = new int[(2 * radius + 1) * (2 * radius + 1)]; //to get the total number of pixels in the neighbourhood
+        int index = 0;
+        for (int j = y - radius; j <= y + radius; j++) {
+            for (int i = x - radius; i <= x + radius; i++) {
+                if (i >= 0 && i < input.getWidth() && j >= 0 && j < input.getHeight()) {
+                    filterSize[index++] = (input.getRGB(i, j) >> shift) & 0xFF;
+                }
+            }
+        }
+        Arrays.sort(filterSize);
+        return filterSize[filterSize.length / 2]; // Return the median value
+    }
 }
