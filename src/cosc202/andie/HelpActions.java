b3204200680e1135b06f6cb816e05957da9cf8fd
Jessica Fan <jessicafan2603@gmail.com>
2024-03-27 15:46:49 +1300

    Made Choose Language Popup Panel

--- src/cosc202/andie/HelpActions.java
+++ src/cosc202/andie/HelpActions.java
@@ -165,7 +165,18 @@ public class HelpActions {
             Locale.setDefault(new Locale(prefs.get("language", "en"), 
                                         prefs.get("country", "NZ")));
 
-            ResourceBundle bundle = ResourceBundle.getBundle("MessageBundle");
+            //ResourceBundle bundle = ResourceBundle.getBundle("MessageBundle");
+        
+            ResourceBundle bundle;
+            try {
+                bundle = loadResourceBundle(Path.of("src/cosc202/andie/"), "MessageBundle");
+            } catch (IOException e1) {
+                System.err.println("Failed to load resource bundle: " + e1.getMessage());
+                // Perform additional error handling or provide a default bundle
+                bundle = ResourceBundle.getBundle("DefaultMessageBundle");
+            }
+
+
             System.out.println(bundle.getString("ChooseLanguageEnglish"));
             String[] options = {bundle.getString("ChooseLanguageEnglish"), bundle.getString("ChooseLanguageSpanish")};
             //String[] options = {"English", "Spanish"};
@@ -190,8 +201,24 @@ public class HelpActions {
                     prefs.put("language", "en");
                     prefs.put("country", "NZ");
                 }
+
+                JOptionPane.showMessageDialog(null, bundle.getString("RestartMessage"),
+                                            bundle.getString("RestartTitle"), JOptionPane.INFORMATION_MESSAGE);
             }  
          }
+
+         private static ResourceBundle loadResourceBundle(Path basePath, String baseName) throws IOException {
+            String bundlePath = basePath + "/" + baseName + "_" + Locale.getDefault().toString() + ".properties";
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(bundlePath);
+                return new PropertyResourceBundle(fis);
+            } finally {
+                if (fis != null) {
+                    fis.close();
+                }
+            }
+        }
     }
 }
 
