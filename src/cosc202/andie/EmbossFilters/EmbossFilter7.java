c16e62c1760df571a731aa4000ba5c220ad49598
krutim12 <krutim06@gmail.com>
2024-05-21 21:03:14 +1200

    convolve class created and emboss filters completed

--- src/cosc202/andie/EmbossFilters/EmbossFilter7.java
+++ src/cosc202/andie/EmbossFilters/EmbossFilter7.java
@@ -2,39 +2,60 @@ package cosc202.andie.EmbossFilters;
 
 
 import java.awt.image.BufferedImage;
-import java.awt.Point;
-import java.awt.image.*;
-import cosc202.andie.AndieConvolveOp;
+
+import cosc202.andie.Convolve;
 import cosc202.andie.ImageOperation;
 
-public class EmbossFilter7 implements ImageOperation, java.io.Serializable {
-    private int x1, x2, y1, y2 = -1;
+import java.awt.image.*;
 
+/*
+ * <p>
+ * A filter that apply emboss to image.
+ * </p>
+ * 
+ * <p>
+ * The emboss filter applies a convolution operation to the image.
+ * </p>
+ * 
+ * @author Kruti Mistry
+ */
+public class EmbossFilter7 implements ImageOperation, java.io.Serializable {
     public EmbossFilter7() {
     }
 
-    EmbossFilter7(Point p1, Point p2) {
-        this.x1 = (int) p1.getX();
-        this.x2 = (int) p2.getX();
-        this.y1 = (int) p1.getY();
-        this.y2 = (int) p2.getY();
-    }
+    /**
+     * <p>
+     * Applies the Emboss filter to an input BufferedImage.
+     * </p>
+     * 
+     * <p>
+     * A convolution is applied to the image using a special kernel. Every pixels
+     * final colour value is influenced by the
+     * neighbouring pixels colour values. The amount a which the neighbours effect
+     * the color is specified in a kernel.
+     * </p>
+     * 
+     * <p>
+     * This emboss goes from top left to bottom right.
+     * </p>
+     * 
+     * @param input The image to apply the filter to.
+     * 
+     * @return Returns the an image with the Emboss filter applied.
+     */
 
     public BufferedImage apply(BufferedImage input) {
-        BufferedImage output = null;
-        try {
-            float[] array = { 0, -1, 0, 0, 0, 0, 0, 1, 0};
-            Kernel kernel = new Kernel(3, 3, array);
-            AndieConvolveOp convOp = new AndieConvolveOp(kernel);
-            output = new BufferedImage(input.getColorModel(), input.copyData(null), input.isAlphaPremultiplied(), null);
-            if (x1 != -1 && x2 != -1 && y1 != -1 && y2 != -1)
-                convOp.filter(input, output, x1, y1, x2, y2);
-            else
-                convOp.filter(input, output);
-        } catch (java.awt.image.RasterFormatException ex) {
-            ex.printStackTrace();
-        }
+        float[] array = { 0, -1, 0, 0, 0, 0, 0, 1, 0};
+
+        Kernel kernel = new Kernel(3, 3, array);
+        System.out.println("Before convolve");
+
+        BufferedImage output = Convolve.filter(input, kernel, true);
+
+        System.out.println("After convolve");
+
         return output;
     }
+
 }
 
